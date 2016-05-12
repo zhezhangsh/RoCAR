1b9137266eded5d92834abb637e07ed0f012682d
Zhang <zhangz@email.chop.edu>
2016-05-12 14:57:02 -0400

    download file

--- R/WorkWithWeb.R
+++ R/WorkWithWeb.R
@@ -1,4 +1,5 @@
 # Download a file via URL to a given directory and return the path to the downloade file
+# When the file is local, copy it to the given path
 DownloadFile<-function(url, path, check.existence=TRUE) {
   # url               The URL to the file to be downloaded
   # path              The path where to save the downloaded file
@@ -8,10 +9,16 @@ DownloadFile<-function(url, path, check.existence=TRUE) {
   require(awsomics);
   require(RoCA);
   
-  if (check.existence & !url.exists(url)) url else {
-    if (!dir.exists(path)) dir.create(path, recursive = TRUE);
-    fn <- paste(path, TrimPath(url), sep='/'); 
-    download.file(url, fn); 
-    fn; 
-  } 
+  if (file.exists(url)) { # actually a local file
+    f0<-TrimPath(url); 
+    fn<-paste(path, fn, sep='/')
+    file.copy(f0, fn); 
+  } else {
+    if (check.existence & !url.exists(url)) url else {
+      if (!dir.exists(path)) dir.create(path, recursive = TRUE);
+      fn <- paste(path, TrimPath(url), sep='/'); 
+      download.file(url, fn); 
+      fn; 
+    }
+  }
 }
