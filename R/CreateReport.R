ff86b4e23c9de694142bd8af94af91ceeb75eeca
Zhang <zhangz@email.chop.edu>
2017-07-03 15:36:13 -0400

    create report

--- R/CreateReport.R
+++ R/CreateReport.R
@@ -63,7 +63,10 @@ CreateReport<-function(yml) {
       roca.message$render<-try({
         ###############################################################################################################
         rmarkdown::render(fn.tmpl, output_format="html_document", output_file="index.html", output_dir=getwd(),       ###
-                          quiet=TRUE, envir=new.env());                                                             ###
+                          quiet=TRUE, envir=new.env());       
+        pandoc_self_contained_html('index.html', 'index_alone.html'); 
+        
+        ###
         # rmarkdown::render(fn.tmpl, output_format="html_document", output_file="index_alone.html", output_dir=getwd(), ###
         #                   output_options=list('self_contained'=TRUE), quiet=TRUE, envir=new.env());                 ###
         ###############################################################################################################
