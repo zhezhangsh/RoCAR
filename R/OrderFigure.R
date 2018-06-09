3ce3aa5325b77dea49afc2026f8f05331a9ee0b3
Zhang <zhangz@email.chop.edu>
2018-06-09 16:23:35 -0400

    figure order

--- R/OrderFigure.R
+++ R/OrderFigure.R
@@ -1,13 +1,13 @@
 # Automatically generate figure order for RoCA reports
 OrderFigure <- function(format=TRUE, reset=FALSE) {
-  if (!exists('figure.count', -1) | reset==TRUE) figure.count <<- -1;
+  if (!exists('figure.count', -1) | reset==TRUE) figure.count <<- 0;
   figure.count <<- figure.count + 1;
   if (!format) figure.count else paste('**Figure ', figure.count, '.**', sep='');
 };
 
 # Automatically generate table order for RoCA reports
 OrderTable <- function(format=TRUE, reset=FALSE) {
-  if (!exists('table.count', -1) | reset==TRUE) table.count <<- -1;
+  if (!exists('table.count', -1) | reset==TRUE) table.count <<- 0;
   table.count <<- table.count + 1;
   if (!format) table.count else paste('**Table ', table.count, '.**', sep='');
 };
